a1571027e1d0922ded43949d84419760daee9e5d
Mathieu Soussignan <mathieu@Ordinateur-portable-de-Mathieu.local>
2024-09-23 16:54:08 +0200

    update différents fichiers

--- src/chasseur.py
+++ src/chasseur.py
@@ -1,22 +1,18 @@
 class Chasseur:
-    def __init__(self, nom, sex, faction, niveau, points_de_vie, armes):
+    def __init__(self, nom, sexe, faction, niveau, points_de_vie, armes):
         self.nom = nom
-        self.sex = sex
+        self.sex = sexe
         self.faction = faction
         self.niveau = niveau
         self.points_de_vie = points_de_vie
         self.armes = armes
 
     def afficher(self):
-        print(f"Nom: {self.nom}")
-        print(f"Sexe: {self.sex}")
-        print(f"Faction: {self.faction}")
-        print(f"Niveau: {self.niveau}")
-        print(f"Points de vie: {self.points_de_vie}")
-        print(f"Armes: {', '.join(self.armes)}")
-        
-    
+        return (f"Le/La Chasseur(e) s'appelle {self.nom}, il/elle est de sexe {self.sexe},"
+                f"il/elle apparait à la faction {self.faction}, il/elle est de niveau {self.niveau},"
+                f"il/elle a {self.points_de_vie} points de vies, et il/elle manie un(e) {self.armes}.")
+          
 
 #initialisation d'un chasseur 
 chasseur1 = Chasseur("Legolas", "Masculin", "Alliance", 9, 110, ["Arc", "Dagues"])
-chasseur1.afficher()
+print(chasseur1.afficher())
