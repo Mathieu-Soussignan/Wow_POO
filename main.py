29b86215afd81238a565872da583858e71ac3ce9
pierre giannucci <pierregia13@gmail.com>
2024-09-24 14:02:26 +0200

    " modif main utilisation getter arme classe perso"

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ from src.guerrier import Guerrier
 from src.magicien import Magicien
 from src.chasseur import Chasseur
 from src.druide import Druide
-
+from src.personnage import Personnage
 def main():
     guerrier = Guerrier("Grommash", "Masculin", "Horde", 60, 3487, "Gorehowl")
     magicien = Magicien("Jaina", "Féminin", "Alliance", 60, 2800, "Bâton de givre")
     chasseur = Chasseur("Rexxar", "Masculin", "Horde", 60, 3100, "Arc long")
     druide = Druide("Malfurion", "Masculin", "Alliance", 60, 3200, "Bâton de la nature")
-
+    
+    perso = Personnage("Zabuza", "Masculin", "Horde", 60, 3000, "kubikiribocho")
+    
     personnages = [guerrier, magicien, chasseur, druide]
-
+    print(f"L'arme de {perso.nom} est : {perso.arme}")  
+    print("-" * 50)
+    
     for personnage in personnages:
         print(personnage.afficher())
         print("-" * 50)
